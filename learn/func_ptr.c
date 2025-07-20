e655a3645701c65bb848ce378e1d296b5eb485b3
wolfkisser-225 <chasemouzi@foxmail.com>
2025-07-21 04:36:22 +0800

    更新.vscode配置

--- learn/func_ptr.c
+++ learn/func_ptr.c
@@ -1,13 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-int add(int a, int b) {
-    return a + b;
-}
 
-int main() {
-    int (*func_ptr)(int, int) = add; // Function pointer declaration and initialization
-    int result = func_ptr(5, 3); // Using the function pointer to call the function
-    printf("Result of addition: %d\n", result);
-    return 0;
-}
